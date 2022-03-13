dbc67aec9dab925a6bd22863178be40813e913e1
MinYoung <MinYoung@DESKTOP-EVAPL33>
2022-03-14 04:09:37 +0900

    This Program is Birthday Management Program

---
+++ src/BMP.java
@@ -0,0 +1,14 @@
+
+public class BMP { //BMP are initials from Birthday Management Program
+		public static void main(String[] args) {
+				System.out.println("1. Enter your Name");
+				System.out.println("2. Enter your Birthday");
+				System.out.println("3. Edit your Name");
+				System.out.println("4. Edit your Birthday");
+				System.out.println("5. Find Birthday by Name");
+				System.out.println("6. Find Name by Birthday");
+				System.out.println("7. Exit");
+				
+		}
+
+}
